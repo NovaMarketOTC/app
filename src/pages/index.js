a58b17e67eaf15ebb797ec24dcc70c33ac32fa16
xTycoonFi <tycoonweb@proton.me>
2023-11-03 17:03:03 +0100

    starter

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+
+  return (
+    <div className="relative max-w-[100vw]">
+      <Head>
+        <title>ETHLisbon</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <h1>HEY</h1>
+    </div>
+  )
+}
